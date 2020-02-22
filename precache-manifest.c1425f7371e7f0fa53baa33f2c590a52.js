44a1271dee7d2542cef2a4e45188912e225b3116
Shiva Krishna Gopannagari <34725150+shivakrishna1995@users.noreply.github.com>
2020-02-22 19:36:35 +0530

    Add files via upload

---
+++ precache-manifest.c1425f7371e7f0fa53baa33f2c590a52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18e93218e96db9307af5604c85bdaefe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d8d2cc17f5baef1766a",
+    "url": "/static/css/2.286a1d7a.chunk.css"
+  },
+  {
+    "revision": "329a9aadc590e99e7a63",
+    "url": "/static/css/main.c083697f.chunk.css"
+  },
+  {
+    "revision": "1d8d2cc17f5baef1766a",
+    "url": "/static/js/2.335231c5.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.335231c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "329a9aadc590e99e7a63",
+    "url": "/static/js/main.59d6171d.chunk.js"
+  },
+  {
+    "revision": "0387a72ba1cd7d3216f0",
+    "url": "/static/js/runtime-main.17c3e8fd.js"
+  }
+]);
